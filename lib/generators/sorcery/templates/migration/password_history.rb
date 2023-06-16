c1818f5c7c6191669bf5ec38b900433198ca03a2
luolixiang <luolixiang@jourdeness.com.tw>
2023-06-16 17:48:26 +0800

    fix column name

--- lib/generators/sorcery/templates/migration/password_history.rb
+++ lib/generators/sorcery/templates/migration/password_history.rb
@@ -1,9 +1,9 @@
 class PasswordHistory < <%= migration_class_name %>
   def change
     create_table :password_history do |t|
-      t.integer user_id,          null: false
-      t.string :crypted_password, null: false
-      t.string :salt,             null: false
+      t.integer :user_id,          null: false
+      t.string  :crypted_password, null: false
+      t.string  :salt,             null: false
 
       t.timestamps                null: false
     end
