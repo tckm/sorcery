cb4ee0cda047f47baa06670416873b85c610eca4
luolixiang <luolixiang@jourdeness.com.tw>
2023-06-16 15:00:52 +0800

    debug

--- lib/sorcery/controller/submodules/change_password_date.rb
+++ lib/sorcery/controller/submodules/change_password_date.rb
@@ -10,22 +10,32 @@ module Sorcery
   
           module InstanceMethods
         
-            def change_password!(old_password, password, password_confirmation)
-                p ['------------------------ ChangePasswordDate.change_password!', old_password, password, password_confirmation]
-
+            def change_password!(old_password, password, password_confirmation)        
                 is_old_password_match = current_user.valid_password?(old_password)
                 is_new_password_match = password == password_confirmation
+
+                p ['------------------------ ChangePasswordDate.change_password!', 
+                    old_password, 
+                    password, 
+                    password_confirmation, 
+                    is_old_password_match,
+                    is_new_password_match
+                ]
 
                 res = false
 
                 if is_old_password_match && is_new_password_match
                     last_change_password_date = Time.now
 
+                    p ['-------------------- current_user', current_user]
+
                     res = current_user.update({
                         password:                  password,
                         password_confirmation:     password_confirmation,
                         last_change_password_date: last_change_password_date
                     })
+
+                    p ['-------------------- res', res]
                 end
 
                 return res
