13da289db6f935cc4922928680bb23bd817c3efa
luolixiang <luolixiang@jourdeness.com.tw>
2023-06-16 15:24:51 +0800

    modify output and remove print

--- lib/sorcery/controller/submodules/change_password_date.rb
+++ lib/sorcery/controller/submodules/change_password_date.rb
@@ -4,7 +4,6 @@ module Sorcery
 
         module ChangePasswordDate
           def self.included(base)
-            p ['---------------- ChangePasswordDate.base', base]
             base.send(:include, InstanceMethods)
           end
   
@@ -13,30 +12,23 @@ module Sorcery
             def change_password!(old_password, password, password_confirmation)        
                 is_old_password_match = current_user.valid_password?(old_password)
                 is_new_password_match = password == password_confirmation
-
-                p ['------------------------ ChangePasswordDate.change_password!', 
-                    old_password, 
-                    password, 
-                    password_confirmation, 
-                    is_old_password_match,
-                    is_new_password_match
-                ]
-
-                res = false
+                update_result         = false
 
                 if is_old_password_match && is_new_password_match
                     last_change_password_date = Time.now
 
-                    p ['-------------------- current_user', current_user]
-
-                    res = current_user.update({
+                    update_result = current_user.update({
                         password:                  password,
                         password_confirmation:     password_confirmation,
                         last_change_password_date: last_change_password_date
                     })
-
-                    p ['-------------------- res', res]
                 end
+
+                res = {
+                    is_old_password_match: is_old_password_match
+                    is_new_password_match: is_new_password_match
+                    update_result:         update_result
+                }
 
                 return res
             end
