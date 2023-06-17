9c1d78e066c2d835fc9a282151ab0b1335bd69cb
luolixiang <luolixiang@jourdeness.com.tw>
2023-06-17 09:04:59 +0800

    controller method to model

--- lib/sorcery/controller/submodules/password_history.rb
+++ lib/sorcery/controller/submodules/password_history.rb
@@ -8,27 +8,6 @@ module Sorcery
           end
   
           module InstanceMethods
-        
-            def add_history!(user)
-                self.password_history.create({
-                    user_id:          user.id,
-                    crypted_password: user.crypted_password,
-                    salt:             user.salt
-                })
-            end
-
-            def in_history?(password)
-                res = false
-
-                self.password_history.each do |item|
-                    check = self.sorcery_config.encryption_provider.matches?(item.crypted_password, password, item.salt)
-                    
-                    return check if chec
-                end
-
-                return res
-            end
-  
           end
         end
       end
--- lib/sorcery/model/submodules/password_history.rb
+++ lib/sorcery/model/submodules/password_history.rb
@@ -9,10 +9,29 @@ module Sorcery
           end
   
           module ClassMethods
- 
+
           end
   
           module InstanceMethods
+            def add_history!()
+              self.password_history.create({
+                  user_id:          self.id,
+                  crypted_password: self.crypted_password,
+                  salt:             self.salt
+              })
+            end
+
+            def in_history?(password)
+              res = false
+
+              self.password_history.each do |item|
+                check = self.sorcery_config.encryption_provider.matches?(item.crypted_password, password, item.salt)
+                    
+                return check if chec
+              end
+
+              return res
+            end
           end
         end
       end
